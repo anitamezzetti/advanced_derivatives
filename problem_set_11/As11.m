80bc6e833d502f6ddf3e3f91c37cf5117e0b5704
Hien <tengsuynh@gmail.com>
2020-12-12 10:20:54 +0100

    ps11

--- problem_set_11/As11.m
+++ problem_set_11/As11.m
@@ -2,12 +2,12 @@
 % for swaption
 t = 0; % first date
 r = 0.05; % term structure (flat)
-Ts = 1; % first ex date
-Tx = 2; % last ex date
+T_s = 1; % first ex date
+T_x = 2; % last ex date
 
 % for underlying swap
 tau = 0.25;
-TN = 4; % last payment date
+T_N = 4; % last payment date
 payment_dates = (0:16).*tau;
 
 % corresponding forward rates
@@ -16,4 +16,70 @@ vols_fw(2:7) = 0.2;
 vols_fw(8:11) = .22;
 vols_fw(12:16) = .24;
 
-deltas = pi/2 * ((1:16) - 2)./14;
+thetas = pi/2 * ((1:16) - 2)./14;
+
+% other params
+N_sim = 10000;
+K = .05;
+N_x = length(tau:tau:T_x);
+N_n = length(tau:tau:T_N);
+Hs = 1e-4:1e-4:r;
+
+%% computation for best H
+
+[F,S] = ForwardRates(r,vols_fw,thetas,K,tau,N_sim,N_x,N_n);
+%disp(S)
+first_ex_date = T_s/tau;
+S = S(:,first_ex_date+1:end);
+%disp(S)
+[~,num_ex_dates] = size(S); 
+best_Hs = zeros(num_ex_dates-1,1);
+
+% price of swaption at 4 dates
+for t=1:4
+    avg_swaption_prices = zeros(length(Hs),1);
+    S_t = S(:,end-t:end);
+    
+    % discounted factor (slide 12 Lecture 11)
+    D_t = cumprod(1./(1+tau*F(:,end-t:end,end-t)),2);
+    %disp(D_t);
+    %if (t==4)
+    %    disp(D_t)
+    %end
+    
+    % experimenting with different H
+    for i=1:length(Hs)
+        CF_H = Swaptions(S_t,Hs(i),D_t);
+        %disp(size(D_t))
+        avg_swaption_prices(i) = mean(CF_H); 
+    end
+    [~,argmax] = max(avg_swaption_prices);
+    best_Hs(t) = Hs(argmax);
+end
+
+%% Generate new paths and compute prices with optimal Hs
+[F,S] = ForwardRates(r,vols_fw,thetas,K,tau,N_sim,N_x,N_n);
+
+first_ex_date = T_s/tau;
+S = S(:,first_ex_date:end);
+[~,num_ex_dates] = size(S); 
+
+SPs = S;
+S_t = S(:,end-4:end);
+D_t = cumprod(1./(1+tau*F(:,end-4:end,end-t)),2);
+CF_t = Swaptions(S_t,best_Hs(4),D_t);
+
+
+% 
+% for t=1:4
+%     S_t = S(:,end-t:end);
+%     
+%     % discounted factor
+%     D_t = cumprod(1./(1+tau*F(:,end-t:end,end-t)),2);
+% 
+%     CF_t = Swaptions(S_t,best_Hs(t),D_t);
+%     %disp(mean(CF_t));
+%     SPs(:,t) = (CF_t);
+% end
+
+%disp("Price of Swaption is", mean(SPs)*10000);
---
+++ problem_set_11/ForwardRates.m
@@ -0,0 +1,44 @@
+function [F, S] = ForwardRates(r, sigma, theta, K, dt, N_sim, N_x, N_n)
+% Dynamics of Forward rates under Spot-LIBOR measure
+
+% N_x: Number of exercise dates
+% N_n: Number of swap payment dates
+
+%dW_k = cos(theta_k) dW_1 + sin(theta_k) dW_2
+
+F = zeros(N_sim, N_x+1, N_n);
+S = zeros(N_sim, N_x+1);
+F(:, 1, :) = r*ones(N_sim, 1, N_n);
+
+for t=2:N_x+1
+    temp = 0;
+    for k=t:N_n
+        dW_1 = randn(N_sim, 1);
+        dW_2 = randn(N_sim, 1);
+        
+        dW_k = (cos(theta(k))*dW_1 + sin(theta(k))*dW_2)*sqrt(dt);
+        
+        % slide 19 Lecture 10
+        rho_tk = cos(theta(k) - theta(t));
+        
+        % slide 6 Lecture 11
+        temp = temp + dt*rho_tk*sigma(k)*F(:,t-1,k)./(1+dt*F(:,t-1,k));
+        mu_k = sigma(k)*temp;
+        F(:,t,k) = F(:,t-1,k).*exp((mu_k - 0.5*sigma(k)^2)*dt +...
+           sigma(k)*dW_k);
+    end
+    %disp(F)
+    % compute value of underlying swap at T_s for an expiry at T_x
+    % Andersen algo
+    P = zeros(N_sim, N_n);
+    
+    if (t > 1/dt)
+        %..
+        P(:, :) = cumprod(1./(1+dt.*F(:,t,:)),3);
+        P(:,1:t-1) = 0;
+        
+        % Value of underlying swap at t (exercise date)
+        S(:, t) = dt*diag(P * (reshape(K-F(:,t,:), [N_sim,N_n]))');
+    end
+end
+end
